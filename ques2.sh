89e251accec7c11996f21bcb08dd7d7695ae063e
rajat1720 <59660948+rajat1720@users.noreply.github.com>
2023-03-23 16:41:05 +0530

    Add files via upload

--- ques2.sh
+++ ques2.sh
@@ -1,21 +1,20 @@
-#!/bin/bash
-read num1 operator num2
-
+read num1 operator num2 
 case $operator in
-    +)
-        result=$(echo "$num1 + $num2" | bc)
-        ;;
-    -)
-        result=$(echo "$num1 - $num2" | bc)
-        ;;
-    \*)
-        result=$(echo "$num1 * $num2" | bc)
-        ;;
-    /)
-        result=$(echo "scale=2; $num1 / $num2" | bc)
-        ;;
-    *)
-        echo "Invalid operator."
-        exit 1
-        ;;
-echo "Result: $result"
++)
+result=$(echo "$num1 + $num2" | bc)
+;;
+-)
+result=$(echo "$num1 - $num2" | bc)
+;;
+\x)
+result=$(echo "$num1 * $num2" | bc)
+;;
+/)
+result=$(echo "scale=2; $num1 / $num2" | bc) 
+;;
+*)
+echo "Invalid operator"
+exit 1
+;;
+esac
+echo "$result"
